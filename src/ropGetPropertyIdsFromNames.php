1c627969cd86a69a757016e855932cbf3c856620
syncgw <syncgw@github.com>
2024-04-15 13:18:19 +0200

    9.20.4

--- src/ropGetPropertyIdsFromNames.php
+++ src/ropGetPropertyIdsFromNames.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
  *
  *	@package	sync*gw
  *	@subpackage	Remote Operations (ROP) List and Encoding Protocol
- *	@copyright	(c) 2008 - 2023 Florian Daeumling, Germany. All right reserved
+ *	@copyright	(c) 2008 - 2024 Florian Daeumling, Germany. All right reserved
  * 	@license 	LGPL-3.0-or-later
  */
 
